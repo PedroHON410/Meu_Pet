f8d8b3889c2a3ce475061a3d3318e682ccd49e8d
PedroHON410 <pedro.hon200511@gmail.com>
2025-06-25 18:23:59 -0300

    adicionando senha e icones

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import PasswordLogin from "./components/PasswordLogin";
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="bg-gradient-to-l from-yellow-400 via-yellow-500 to-orange-400 min-h-screen w-screen flex items-center justify-center relative overflow-hidden">
       <div className="flex flex-col items-center w-full max-w-md px-6 ">
+=======
+    <div class="bg-gradient-to-l from-yellow-500 via-yellow-600 to-orange-500 h-screen w-screen flex justify-center p-6 space-y-5">
+      <div>
+>>>>>>> dcfbdfd (Adição de um readme e imagens)
         <h1
           className="text-teal-500 text-8xl font-bold text-center "
           style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 1)" }}
