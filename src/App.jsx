6abfc9c48103de05289b791a12b70127e5b37e30
PedroHON410 <pedro.hon200511@gmail.com>
2025-07-02 19:48:58 -0300

    Fazendo mais estilizações

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <header className="py-6 text-center">
         <h1
           className="text-teal-500 text-8xl font-bold text-center py-3  "
-          style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 1)" }}
+          style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.5)" }}
         >
           <a href="">MEU PET</a>
         </h1>
@@ -18,14 +18,19 @@ function App() {
         <div className="w-full max-w-md space-y-20 px-4 relative z-40 ">
           <Userlogin />
           <PasswordLogin />
-          <div className="border text-border- bg-teal-500 text-center text-white font-bold text-2xl  px-4 py-2 rounded-md">
-            <a href="http://localhost:5173/Create_count">Cadastre-se agora!</a>
+          <div className="border bg-[#01bbb9] text-white px-6 py-4 rounded-lg shadow-md transform transition-transform duration-300 hover:scale-110 hover:shadow-xl">
+            <a
+              href="http://localhost:5173/Create_count "
+              className="flex justify-center font-bold text-2xl text-shadow-lg shadow-black"
+            >
+              Cadastre-se agora!
+            </a>
           </div>
         </div>
         <img
           src={pata1}
           alt="pata"
-          className=" absolute w-11/12  left-12 scale-50 flex justify-center"
+          className=" absolute w-11/12  left-12 scale-50 flex justify-center "
         />
         <img
           src={pata2}
--- src/pages/Createcount.jsx
+++ src/pages/Createcount.jsx
@@ -3,12 +3,15 @@ function Createcount() {
     <div className="bg-gradient-to-l from-yellow-400 via-yellow-500 to-orange-400 min-h-screen w-screen flex flex-col items-center">
       <h1
         className="text-teal-500 text-8xl font-bold text-center mt-10 mb-10 py-8"
-        style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 1)" }}
+        style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.5)" }}
       >
         Cadastro de Conta
       </h1>
       <div className="bg-teal-500 rounded-lg shadow-lg px-8 py-52 shadow-black w-full max-w-md my-20 ">
-        <h1></h1>
+        <h2 className="text-white text-3xl font-bold mb-6 text-center shadow-black">
+          Digite seu nome completo
+        </h2>
+        <input type="text" className="" />
       </div>
     </div>
   );
