3e613d23613436f9deeb7111d76ace1e22605064
PedroHON410 <pedro.hon200511@gmail.com>
2025-07-09 17:38:35 -0300

    Adicionando usestates para os inputs e personalizando o botão

--- src/pages/Createcount.jsx
+++ src/pages/Createcount.jsx
@@ -2,53 +2,77 @@ import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 function Createcount() {
   const [ShowPassword, setShowPassword] = useState(false);
+  const [Name, setName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Nome:", Name);
+    console.log("Email:", Email);
+    console.log("Senha:", Password);
+  };
   return (
     <div className="bg-gradient-to-l from-yellow-400 via-yellow-500 to-orange-400 min-h-screen w-screen flex flex-col items-center">
-      <h1
-        className="text-teal-500 text-8xl font-bold text-center mt-10 mb-10 py-8"
-        style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.5)" }}
-      >
-        Cadastro de Conta
-      </h1>
-      <div className="bg-teal-500 rounded-lg shadow-lg px-8 py-44 shadow-black w-full max-w-md my-12 flex flex-col ">
-        <h2
-          className=" text-white text-3xl font-bold mb-6 text-shadow-2xs text-shadow-sky-300 text-center -my-36  "
+      <form onSubmit={handleSubmit} className="w-full max-w-md">
+        <h1
+          className="text-teal-500 text-8xl font-bold text-center mt-10 mb-10 py-8"
           style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.5)" }}
         >
-          Digite seu nome
-        </h2>
-        <input type="text" className="rounded-md py-2 px-4 outline-teal-500" />
+          Cadastro de Conta
+        </h1>
+        <div className="bg-teal-500 rounded-lg shadow-lg px-8 py-44 shadow-black w-full max-w-md my-12 flex flex-col ">
+          <h2
+            className=" text-white text-3xl font-bold mb-6 text-shadow-2xs text-shadow-sky-300 text-center -my-36  "
+            style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.5)" }}
+          >
+            Digite seu nome
+          </h2>
+          <input
+            type="text"
+            className="rounded-md py-2 px-4 outline-teal-500"
+            onChange={(e) => setName(e.target.value)}
+          />
 
-        <h2
-          className="text-white text-3xl font-bold mb-6  shadow-black text-center my-6"
-          style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.5)" }}
-        >
-          Digite seu email
-        </h2>
-        <input type="email" className="rounded-md py-2 px-4 outline-teal-500" />
-
-        <div className="relative w-full">
           <h2
             className="text-white text-3xl font-bold mb-6  shadow-black text-center my-6"
             style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.5)" }}
           >
-            Crie uma senha
+            Digite seu email
           </h2>
-          <div className="flex ">
-            <input
-              type={ShowPassword ? "text" : "password"}
-              className="rounded-l-lg py-2 pr-40 outline-teal-500 text-left"
-            />
-            <button
-              type="button"
-              className=" text-teal-500 bg-white py-2 px-2 rounded-r-lg"
-              onClick={() => setShowPassword(!ShowPassword)}
+          <input
+            type="email"
+            className="rounded-md py-2 px-4 outline-teal-500"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <div className="relative w-full">
+            <h2
+              className="text-white text-3xl font-bold mb-6  shadow-black text-center my-6"
+              style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.5)" }}
             >
-              {ShowPassword ? <Eye /> : <EyeOff />}
-            </button>
+              Crie uma senha
+            </h2>
+            <div className="flex ">
+              <input
+                type={ShowPassword ? "text" : "password"}
+                className="rounded-l-lg py-2 pr-40 outline-teal-500 text-left"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button
+                type="button"
+                className=" text-teal-500 bg-white py-2 px-2 rounded-r-lg"
+                onClick={() => setShowPassword(!ShowPassword)}
+              >
+                {ShowPassword ? <Eye /> : <EyeOff />}
+              </button>
+            </div>
           </div>
+          <button className="my-9 bg-teal-500 text-white font-bold text-2xl border rounded-lg py-3 px-3  hover:text-teal-500 hover:bg-white  transition-colors duration-300 shadow-md  transform  hover:scale-110 hover:shadow-xl-white hover:border-yellow-400">
+            Criar
+          </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
